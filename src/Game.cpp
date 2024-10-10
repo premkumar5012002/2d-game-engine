edcf864dbd6e79ab1f112c80facf56b8491c7876
Prem Kumar <premkumar5012002@gmail.com>
2024-10-10 05:31:31 +0000

    setup game loop

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,9 +2,12 @@
 
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <glm/glm.hpp>
+#include <SDL2/SDL_image.h>
 
 Game::Game() {
     isRunning = false;
+    millisecondsPreviousFrame = 0;
 }
 
 Game::~Game() {
@@ -47,6 +50,7 @@ void Game::Initialize() {
 }
 
 void Game::Run() {
+    Setup(); 
     while(isRunning) {
         ProcessInput();
         Update();
@@ -54,7 +58,16 @@ void Game::Run() {
     }
 }
 
-void Game::ProcessInput() {
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
+
+void Game::Setup() {
+    playerPosition = glm::vec2(10.0, 20.0);
+    playerVelocity = glm::vec2(10.0, 5.0);
+}
+
+
+void Game::ProcessInput() { 
     SDL_Event event;
 
     while (SDL_PollEvent(&event)) {
@@ -73,14 +86,43 @@ void Game::ProcessInput() {
 }
 
 void Game::Update() {
-    // TODO: Update game objects...
+    int timeToWait = MILLISECONDS_PER_FRAME - (SDL_GetTicks64() - millisecondsPreviousFrame);
+    if (timeToWait > 0 && timeToWait <= MILLISECONDS_PER_FRAME) {
+        SDL_Delay(timeToWait);
+    }
+
+    // Difference in ticks from last frame, coverted to seconds
+    double deltaTime = (SDL_GetTicks64() - millisecondsPreviousFrame) / 1000.0;
+
+    // Store the current frame time in previous frame for next frame update
+    millisecondsPreviousFrame = SDL_GetTicks64();
+
+    playerPosition.x += playerVelocity.x * deltaTime;
+    playerPosition.y += playerVelocity.y * deltaTime;
 }
 
 void Game::Render() {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
     SDL_RenderClear(renderer);
 
-    // TODO: Render all game objects...
+    // Draw a PNG texture
+    SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    // Free surface after loaded into texture
+    SDL_FreeSurface(surface);
+
+    // What is the destination rectangle that we want to place our texture
+    SDL_Rect dstRect = {
+        static_cast<int>(playerPosition.x), 
+        static_cast<int>(playerPosition.y), 
+        32, 
+        32 
+    };
+    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+    
+    // Destroy texture after loaded into renderer
+    SDL_DestroyTexture(texture);
 
     SDL_RenderPresent(renderer);
 }
--- src/Game.h
+++ src/Game.h
@@ -2,12 +2,17 @@
 
 #include <SDL2/SDL.h>
 
+const int FPS = 75;
+const int MILLISECONDS_PER_FRAME = 1000 / FPS; 
+
 class Game {
     private:
         bool isRunning;
+        int millisecondsPreviousFrame;
         SDL_Window* window;
         SDL_Renderer* renderer;
 
+        void Setup();
         void ProcessInput();
         void Update();
         void Render();
